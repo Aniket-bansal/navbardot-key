c41f97c42b4f0c9d38a332f9bcd57442b94e48ca
Aniket bansal <77097101+Aniket-bansal@users.noreply.github.com>
2022-06-15 18:05:50 +0530

    Add files via upload

---
+++ personal/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Footer from './HomePageComponents/Footer';
+import Navbar from './NavbarComponents/Navbar';
+import Contact from './NavbarComponents/components/Contact.jsx'
+import About from './NavbarComponents/components/About';
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+     
+    </div>
+  );
+}
+
+export default App;
